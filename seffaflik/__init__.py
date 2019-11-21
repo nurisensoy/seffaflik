0d4f2d835ee3a04a4a6c35313cc06b05a1ee44a1
nuri.sensoy <nuri.sensoy@epias.com.tr>
2019-11-21 17:05:40 +0300

    Changed __init__ to make classes in files callable

---
+++ seffaflik/__init__.py
@@ -0,0 +1,14 @@
+from seffaflik.elektrik.uretim.uretim import eak, gerceklesen, kgup, kudup, organizasyon_cekis_birimleri, \
+    organizasyonlar, santral_cekis_birimleri, tum_organizasyonlar_cekis_birimleri, tum_organizasyonlar_eak, \
+    tum_organizasyonlar_kgup
+from seffaflik.elektrik.uretim.santraller import ariza_bakim_bildirimleri, kurulu_guc, santraller, yekdem_kurulu_guc, \
+    yekdem_santralleri
+from seffaflik.elektrik.tuketim.tuketim import dagitim_bolgeleri, gerceklesen, serbest_tuketici_sayisi, tahmin, uecm, \
+    uecm_serbest_tuketici_donemsel, uecm_tedarik
+from seffaflik.elektrik.piyasalar.dgp import hacim, smf
+from seffaflik.elektrik.piyasalar.gip import aof, eslesme_miktari, islem_akisi, islem_hacmi, min_max_fiyatlar, ozet, \
+    teklif_edilen_miktarlar
+from seffaflik.elektrik.piyasalar.gop import arz_talep_egrisi, blok_miktari, fark_tutari, hacim, islem_hacmi, kptf, \
+    organizasyonel_hacim, ptf, tum_organizasyonlar_arz_hacim, tum_organizasyonlar_net_hacim, \
+    tum_organizasyonlar_talep_hacim
+from seffaflik.elektrik.piyasalar.ia import hacim, organizasyonel_hacim, tum_organizasyonlar_hacim

4ce557521e4138ed3f631513778ef7ce3e7ec0d8
nuri.sensoy <nuri.sensoy@epias.com.tr>
2019-11-25 16:12:40 +0300

    __baslangic_bitis_tarih_eic_dogrulama() is added

--- seffaflik/ortak/dogrulama.py
+++ seffaflik/ortak/dogrulama.py
@@ -2,12 +2,30 @@ import datetime as __datetime
 import logging as __logging
 from seffaflik.ortak import parametreler as __param
 
+
 def __baslangic_bitis_tarih_dogrulama(baslangic_tarihi, bitis_tarihi):
     try:
         ilk = __datetime.datetime.strptime(baslangic_tarihi, '%Y-%m-%d')
         son = __datetime.datetime.strptime(bitis_tarihi, '%Y-%m-%d')
         if ilk > son:
             __logging.warning("Bitiş tarihi başlangıç tarihinden küçük olmamalıdır!")
+            return False
+    except ValueError:
+        __logging.warning("Girilen tarihler YYYY-AA-GG formatında olmalıdır! (Ör: 2000-10-25)")
+        return False
+    else:
+        return True
+
+
+def __baslangic_bitis_tarih_eic_dogrulama(baslangic_tarihi, bitis_tarihi, eic):
+    try:
+        ilk = __datetime.datetime.strptime(baslangic_tarihi, '%Y-%m-%d')
+        son = __datetime.datetime.strptime(bitis_tarihi, '%Y-%m-%d')
+        if ilk > son:
+            __logging.warning("Bitiş tarihi başlangıç tarihinden küçük olmamalıdır!")
+            return False
+        if not isinstance(eic, str):
+            __logging.warning("Organizasyon EIC değeri metin formatında girilmelidir!")
             return False
     except ValueError:
         __logging.warning("Girilen tarihler YYYY-AA-GG formatında olmalıdır! (Ör: 2000-10-25)")
@@ -26,17 +44,17 @@ def __tarih_dogrulama(tarih):
         return True
 
 
-def __check_HTTPError(error):
+def __check_http_error(error):
     if error == 401:
         __logging.error(__param.__requestsAuthenticationErrorLogging, exc_info=False)
 
 
 def __kgup_girebilen_organizasyon_dogrulama(eic):
-    if type(eic) != str:
+    if not isinstance(eic, str):
         __logging.warning("EIC değeri metin formatında girilmelidir!")
         return False
     else:
-        from seffaflik.elektrik.uretim import uretim
+        from seffaflik.elektrik import uretim
         df_org = uretim.organizasyonlar()
         if eic in df_org["EIC Kodu"].values:
             return True

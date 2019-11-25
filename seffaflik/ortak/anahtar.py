c111102b7c05fecd18025478f307188345abda21
nuri.sensoy <nuri.sensoy@epias.com.tr>
2019-11-25 16:06:39 +0300

    kimlik_dosyasi_olustur() is moved from araclar to anahtar

--- seffaflik/ortak/anahtar.py
+++ seffaflik/ortak/anahtar.py
@@ -1,5 +1,6 @@
 import os as __os
 import json as __json
+# from requests.compat import json as __json
 
 HEADERS = {
     'x-ibm-client-id': "",
@@ -12,3 +13,20 @@ if __os.path.exists(kimlik_dosyasi):
         kimlik = f.read()
         istemci_taniticisi = __json.loads(kimlik)["istemci_taniticisi"]
         HEADERS["x-ibm-client-id"] = istemci_taniticisi
+
+
+def kimlik_dosyasi_olustur(istemci_taniticisi):
+    """Api anahtarını `~/.seffaflik/.kimlik` içerisinde oluşturur.
+
+    :param istemci_taniticisi: Şeffaflık Platformuna kayıt olurken verilen api anahtarı
+    """
+    HEADERS["x-ibm-client-id"] = istemci_taniticisi
+    seffaflik_dir = __os.environ.get("SEFFAFLIK_DIR",
+                                     __os.path.join(__os.path.expanduser("~"), ".seffaflik"))
+    if not __os.path.exists(seffaflik_dir):
+        __os.makedirs(seffaflik_dir)
+    kimlik_dosyasi = __os.path.join(seffaflik_dir, ".kimlik")
+    kimlik_bilgisi = {"istemci_taniticisi": istemci_taniticisi}
+    json_string = __json.dumps(kimlik_bilgisi, indent=4)
+    with open(kimlik_dosyasi, "w") as f:
+        f.write(json_string)
--- seffaflik/ortak/araclar.py
+++ seffaflik/ortak/araclar.py
@@ -1,8 +1,4 @@
-import os as __os
-from requests.compat import json as __json
 import pandas as __pd
-
-from seffaflik.ortak.anahtar import HEADERS
 
 
 def __merge_ia_dfs_evenif_empty(df_arz, df_talep):
@@ -57,20 +53,3 @@ def __change_df_eic_column_names_with_short_names(df, org):
     new_column_names.insert(0, "Tarih")
     df.columns = new_column_names
     return df
-
-
-def kimlik_dosyasi_olustur(istemci_taniticisi):
-    """Kullanıcı adı ve api anahtar ikilisini `~/.seffaflik/.kimlik` içerisinde oluşturur.
-
-    :param istemci_taniticisi: Şeffaflık Platformuna kayıt olurken verilen api anahtarı
-    """
-    HEADERS["x-ibm-client-id"] = istemci_taniticisi
-    seffaflik_dir = __os.environ.get("SEFFAFLIK_DIR",
-                                     __os.path.join(__os.path.expanduser("~"), ".seffaflik"))
-    if not __os.path.exists(seffaflik_dir):
-        __os.makedirs(seffaflik_dir)
-    kimlik_dosyasi = __os.path.join(seffaflik_dir, ".kimlik")
-    kimlik_bilgisi = {"istemci_taniticisi": istemci_taniticisi}
-    json_string = __json.dumps(kimlik_bilgisi, indent=4)
-    with open(kimlik_dosyasi, "w") as f:
-        f.write(json_string)

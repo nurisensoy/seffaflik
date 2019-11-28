7fa88c14b726b1863f9eb2865008f3ec80a4d8aa
nuri.sensoy <nuri.sensoy@epias.com.tr>
2019-11-28 19:48:11 +0300

    1) hourly volume is added to hacim(). 2) fiyat() is added to fetch PTF, AOF, SMF together

--- seffaflik/elektrik/piyasalar/genel.py
+++ seffaflik/elektrik/piyasalar/genel.py
@@ -1,5 +1,4 @@
 import requests as __requests
-import itertools as __itertools
 from requests import ConnectionError as __ConnectionError
 from requests.exceptions import HTTPError as __HTTPError, RequestException as __RequestException, Timeout as __Timeout
 import pandas as __pd
@@ -7,8 +6,10 @@ import datetime as __dt
 from multiprocessing import Pool as __Pool
 import multiprocessing as __mp
 from dateutil import relativedelta as __rd
+from functools import reduce as __red
 import logging as __logging
 
+from seffaflik.elektrik.piyasalar import ia as __ia, gop as __gop, gip as __gip, dgp as __dgp
 from seffaflik.ortak import dogrulama as __dogrulama, parametreler as __param, anahtar as __api
 
 __transparency_url = __param.SEFFAFLIK_URL + "market/"
@@ -18,7 +19,7 @@ __headers = __api.HEADERS
 def katilimci_sayisi(baslangic_tarihi=__dt.datetime.today().strftime("%Y-%m-%d"),
                      bitis_tarihi=__dt.datetime.today().strftime("%Y-%m-%d")):
     """
-    İlgili tarih aralığına tekabül eden aylar için EPİAŞ sistemine kayıtlı katılımcıların lisans tipine göre sayısını
+    İlgili tarih aralığına tekabül eden dönemler için EPİAŞ sistemine kayıtlı katılımcıların lisans tipine göre sayısını
     vermektedir.
 
     Parametreler
@@ -42,10 +43,10 @@ def katilimci_sayisi(baslangic_tarihi=__dt.datetime.today().strftime("%Y-%m-%d")
         return __pd.concat(df_list, sort=False)
 
 
-def piyasa_hacimleri(baslangic_tarihi=__dt.datetime.today().strftime("%Y-%m-%d"),
-                     bitis_tarihi=__dt.datetime.today().strftime("%Y-%m-%d"), periyot="günlük"):
+def hacim(baslangic_tarihi=__dt.datetime.today().strftime("%Y-%m-%d"),
+          bitis_tarihi=__dt.datetime.today().strftime("%Y-%m-%d"), periyot="gunluk"):
     """
-    İlgili tarih aralığı için saatlik GÖP, GİP, DGP, İA ticaret hacmi bilgilerini vermektedir.
+    İlgili tarih aralığı için GÖP, GİP, DGP, İA hacim bilgilerini vermektedir.
 
     Parametreler
     ------------
@@ -57,32 +58,100 @@ def piyasa_hacimleri(baslangic_tarihi=__dt.datetime.today().strftime("%Y-%m-%d")
     -----------------
     GÖP, GİP, DGP, İA Hacimleri (MWh)
     """
-    if __dogrulama.__baslangic_bitis_tarih_dogrulama(baslangic_tarihi, bitis_tarihi):
-        try:
-            resp = __requests.get(
-                __transparency_url + "market-volume" + "?startDate=" + baslangic_tarihi + "&endDate=" + bitis_tarihi +
-                "&period=" + __param.translations[periyot], headers=__headers, timeout=__param.__timeout)
-            resp.raise_for_status()
-            list_hacim = resp.json()["body"]["marketVolumeList"]
-            df_hacim = __pd.DataFrame(list_hacim)
-            df_hacim["Tarih"] = __pd.to_datetime(df_hacim["date"].apply(lambda d: d[:10]))
-            df_hacim.rename(index=str,
-                            columns={"bilateralContractAmount": "İA Miktarı", "dayAheadMarketVolume": "GÖP Mİktarı",
-                                     "intradayVolume": "GİP Mİktarı", "balancedPowerMarketVolume": "DGP Miktarı"},
-                            inplace=True)
-            df_hacim = df_hacim[["Tarih", "İA Miktarı", "GÖP Mİktarı", "GİP Mİktarı", "DGP Miktarı"]]
-        except __ConnectionError:
-            __logging.error(__param.__requestsConnectionErrorLogging, exc_info=False)
-        except __Timeout:
-            __logging.error(__param.__requestsTimeoutErrorLogging, exc_info=False)
-        except __HTTPError as e:
-            __dogrulama.__check_http_error(e.response.status_code)
-        except __RequestException:
-            __logging.error(__param.__request_error, exc_info=False)
-        except KeyError:
-            return __pd.DataFrame()
-        else:
+    if __dogrulama.__baslangic_bitis_tarih__periyot_dogrulama(baslangic_tarihi, bitis_tarihi, periyot):
+        if periyot.lower() == "saatlik":
+            df_ia = __ia.hacim(baslangic_tarihi, bitis_tarihi)[["Tarih", "Saat", "Arz Miktarı"]]
+            df_ia.rename(index=str, columns={"Arz Miktarı": "İA"}, inplace=True)
+            df_gop = __gop.hacim(baslangic_tarihi, bitis_tarihi)[["Tarih", "Saat", "Eşleşme Miktarı"]]
+            df_gop.rename(index=str, columns={"Eşleşme Miktarı": "GÖP"}, inplace=True)
+            df_gip = __gip.hacim(baslangic_tarihi, bitis_tarihi)[["Tarih", "Saat", "Eşleşme Miktarı"]]
+            df_gip.rename(index=str, columns={"Eşleşme Miktarı": "GİP"}, inplace=True)
+            df_dgp = __dgp.hacim(baslangic_tarihi, bitis_tarihi)
+            df_dgp["Toplam"] = df_dgp["Teslim Edilen YAL"] - df_dgp["Teslim Edilen YAT"]
+            df_dgp = df_dgp[["Tarih", "Saat", "Toplam"]]
+            df_dgp.rename(index=str, columns={"Toplam": "DGP"}, inplace=True)
+            df_hacim = __red(lambda left, right: __pd.merge(left, right, on=["Tarih", "Saat"]),
+                             [df_ia, df_gop, df_gip, df_dgp])
             return df_hacim
+        else:
+            periods = {"gunluk": "DAILY", "aylik": "MONTHLY", "yillik": "YEAR"}
+            try:
+                resp = __requests.get(
+                    __transparency_url + "market-volume" + "?startDate=" + baslangic_tarihi + "&endDate=" + bitis_tarihi
+                    + "&period=" + periods[periyot.lower()], headers=__headers, timeout=__param.__timeout)
+                resp.raise_for_status()
+                list_hacim = resp.json()["body"]["marketVolumeList"]
+                df_hacim = __pd.DataFrame(list_hacim)
+                df_hacim["Tarih"] = __pd.to_datetime(df_hacim["date"].apply(lambda d: d[:10]))
+                df_hacim.rename(index=str,
+                                columns={"bilateralContractAmount": "İA Miktarı", "dayAheadMarketVolume": "GÖP Mİktarı",
+                                         "intradayVolume": "GİP Mİktarı", "balancedPowerMarketVolume": "DGP Miktarı"},
+                                inplace=True)
+                df_hacim = df_hacim[["Tarih", "İA Miktarı", "GÖP Mİktarı", "GİP Mİktarı", "DGP Miktarı"]]
+            except __ConnectionError:
+                __logging.error(__param.__requestsConnectionErrorLogging, exc_info=False)
+            except __Timeout:
+                __logging.error(__param.__requestsTimeoutErrorLogging, exc_info=False)
+            except __HTTPError as e:
+                __dogrulama.__check_http_error(e.response.status_code)
+            except __RequestException:
+                __logging.error(__param.__request_error, exc_info=False)
+            except KeyError:
+                return __pd.DataFrame()
+            else:
+                return df_hacim
+
+
+def fiyat(baslangic_tarihi=__dt.datetime.today().strftime("%Y-%m-%d"),
+          bitis_tarihi=__dt.datetime.today().strftime("%Y-%m-%d"), periyot="saatlik"):
+    """
+    İlgili tarih aralığı için saatlik GÖP, GİP, DGP fiyat bilgilerini vermektedir.
+
+    Parametreler
+    ------------
+    baslangic_tarihi : %YYYY-%AA-%GG formatında başlangıç tarihi (Varsayılan: bugün)
+    bitis_tarihi     : %YYYY-%AA-%GG formatında bitiş tarihi (Varsayılan: bugün)
+    periyot          : metin formatında periyot(saatlik, günlük, aylik, yillik) (Varsayılan: "günlük")
+
+    Geri Dönüş Değeri
+    -----------------
+    GÖP, GİP, DGP Fiyatları (MWh)
+    """
+    if __dogrulama.__baslangic_bitis_tarih__periyot_dogrulama(baslangic_tarihi, bitis_tarihi, periyot):
+        if periyot.lower() == "saatlik":
+            df_gop = __gop.ptf(baslangic_tarihi, bitis_tarihi)
+            df_gip = __gip.aof(baslangic_tarihi, bitis_tarihi)
+            df_dgp = __dgp.smf(baslangic_tarihi, bitis_tarihi)[["Tarih", "Saat", "SMF"]]
+            df_fiyat = __red(lambda left, right: __pd.merge(left, right, on=["Tarih", "Saat"]),
+                             [df_gop, df_gip, df_dgp])
+            return df_fiyat
+        else:
+            periods = {"gunluk": "DAILY", "aylik": "MONTHLY", "yillik": "YEAR"}
+            try:
+                resp = __requests.get(
+                    __transparency_url + "market-volume" + "?startDate=" + baslangic_tarihi + "&endDate=" + bitis_tarihi
+                    + "&period=" + periods[periyot.lower()], headers=__headers, timeout=__param.__timeout)
+                resp.raise_for_status()
+                list_hacim = resp.json()["body"]["marketVolumeList"]
+                df_hacim = __pd.DataFrame(list_hacim)
+                df_hacim["Tarih"] = __pd.to_datetime(df_hacim["date"].apply(lambda d: d[:10]))
+                df_hacim.rename(index=str,
+                                columns={"bilateralContractAmount": "İA Miktarı", "dayAheadMarketVolume": "GÖP Mİktarı",
+                                         "intradayVolume": "GİP Mİktarı", "balancedPowerMarketVolume": "DGP Miktarı"},
+                                inplace=True)
+                df_hacim = df_hacim[["Tarih", "İA Miktarı", "GÖP Mİktarı", "GİP Mİktarı", "DGP Miktarı"]]
+            except __ConnectionError:
+                __logging.error(__param.__requestsConnectionErrorLogging, exc_info=False)
+            except __Timeout:
+                __logging.error(__param.__requestsTimeoutErrorLogging, exc_info=False)
+            except __HTTPError as e:
+                __dogrulama.__check_http_error(e.response.status_code)
+            except __RequestException:
+                __logging.error(__param.__request_error, exc_info=False)
+            except KeyError:
+                return __pd.DataFrame()
+            else:
+                return df_hacim
 
 
 def __katilimci_sayisi(tarih):
@@ -103,11 +172,11 @@ def __katilimci_sayisi(tarih):
         resp.raise_for_status()
         list_katilimci = resp.json()["body"]["participantList"]
         df_katilimci = __pd.DataFrame(list_katilimci)
-        tuples = list(__itertools.product(["Özel Sektör", "Kamu Kuruluşu"], list(df_katilimci["licence"]) + ["Toplam"]))
-        index = __pd.MultiIndex.from_tuples(tuples, names=['', 'Lisans Tipi'])
+        columns = __pd.MultiIndex.from_product(
+            [["Özel Sektör", "Kamu Kuruluşu"], list(df_katilimci["licence"]) + ["Toplam"]], names=['', ''])
         df_katilimci = __pd.DataFrame([list(df_katilimci["privateSector"]) + list(
             df_katilimci["privateSectorOfSum"].unique()) + list(df_katilimci["publicCompany"]) + list(
-            df_katilimci["publicCompanyOfSum"].unique())], index=[tarih], columns=index)
+            df_katilimci["publicCompanyOfSum"].unique())], index=[tarih], columns=columns)
         df_katilimci["Toplam"] = df_katilimci["Kamu Kuruluşu"]["Toplam"] + df_katilimci["Özel Sektör"]["Toplam"]
     except __ConnectionError:
         __logging.error(__param.__requestsConnectionErrorLogging, exc_info=False)

b9f2b7ae95742e82b4152ea9eaeffdabf3f7ac33
nuri.sensoy <nuri.sensoy@epias.com.tr>
2019-11-28 17:18:35 +0300

    remove dengesizlik_miktarı, dengesizlik_fiyati since dengesizlik gives both of them

--- seffaflik/elektrik/piyasalar/dengesizlik.py
+++ seffaflik/elektrik/piyasalar/dengesizlik.py
@@ -11,96 +11,11 @@ __transparency_url = __param.SEFFAFLIK_URL + "market/"
 __headers = __api.HEADERS
 
 
-def dengesizlik_miktari(baslangic_tarihi=__dt.datetime.today().strftime("%Y-%m-%d"),
-                        bitis_tarihi=__dt.datetime.today().strftime("%Y-%m-%d")):
+def dengesizlik(baslangic_tarihi=__dt.datetime.today().strftime("%Y-%m-%d"),
+                bitis_tarihi=__dt.datetime.today().strftime("%Y-%m-%d")):
     """
-    İlgili tarih aralığı için saatlik dengesizlik miktarını vermektedir.
-
-    Parametreler
-    ------------
-    baslangic_tarihi : %YYYY-%AA-%GG formatında başlangıç tarihi (Varsayılan: bugün)
-    bitis_tarihi     : %YYYY-%AA-%GG formatında bitiş tarihi (Varsayılan: bugün)
-
-    Geri Dönüş Değeri
-    -----------------
-    Dengesizlik Miktarı (MWh)
-    """
-    if __dogrulama.__baslangic_bitis_tarih_dogrulama(baslangic_tarihi, bitis_tarihi):
-        try:
-            resp = __requests.get(
-                __transparency_url + "imbalance-quantity" + "?startDate=" + baslangic_tarihi + "&endDate=" +
-                bitis_tarihi, headers=__headers, timeout=__param.__timeout)
-            resp.raise_for_status()
-            list_dengesizlik = resp.json()["body"]["imbalanceQuantityList"]
-            df_dengesizlik = __pd.DataFrame(list_dengesizlik)
-            df_dengesizlik["Saat"] = df_dengesizlik["date"].apply(lambda h: int(h[11:13]))
-            df_dengesizlik["Tarih"] = __pd.to_datetime(df_dengesizlik["date"].apply(lambda d: d[:10]))
-            df_dengesizlik.rename(index=str, columns={"positiveImbalance": "Pozitif Dengesizlik Miktarı (MWh)",
-                                                      "negativeImbalance": "Negatif Dengesizlik Miktarı (MWh)"},
-                                  inplace=True)
-            df_dengesizlik = df_dengesizlik[
-                ["Tarih", "Saat", "Pozitif Dengesizlik Miktarı (MWh)", "Negatif Dengesizlik Miktarı (MWh)"]]
-        except __ConnectionError:
-            __logging.error(__param.__requestsConnectionErrorLogging, exc_info=False)
-        except __Timeout:
-            __logging.error(__param.__requestsTimeoutErrorLogging, exc_info=False)
-        except __HTTPError as e:
-            __dogrulama.__check_http_error(e.response.status_code)
-        except __RequestException:
-            __logging.error(__param.__request_error, exc_info=False)
-        except KeyError:
-            return __pd.DataFrame()
-        else:
-            return df_dengesizlik
-
-
-def dengesizlik_tutari(baslangic_tarihi=__dt.datetime.today().strftime("%Y-%m-%d"),
-                       bitis_tarihi=__dt.datetime.today().strftime("%Y-%m-%d")):
-    """
-    İlgili tarih aralığı için saatlik dengesizlik tutarını vermektedir.
-
-    Parametreler
-    ------------
-    baslangic_tarihi : %YYYY-%AA-%GG formatında başlangıç tarihi (Varsayılan: bugün)
-    bitis_tarihi     : %YYYY-%AA-%GG formatında bitiş tarihi (Varsayılan: bugün)
-
-    Geri Dönüş Değeri
-    -----------------
-    Dengesizlik Tutarı (TL)
-    """
-    if __dogrulama.__baslangic_bitis_tarih_dogrulama(baslangic_tarihi, bitis_tarihi):
-        try:
-            resp = __requests.get(
-                __transparency_url + "imbalance-amount" + "?startDate=" + baslangic_tarihi + "&endDate=" + bitis_tarihi,
-                headers=__headers, timeout=__param.__timeout)
-            resp.raise_for_status()
-            list_dengesizlik = resp.json()["body"]["imbalanceAmountList"]
-            df_dengesizlik = __pd.DataFrame(list_dengesizlik)
-            df_dengesizlik["Saat"] = df_dengesizlik["date"].apply(lambda h: int(h[11:13]))
-            df_dengesizlik["Tarih"] = __pd.to_datetime(df_dengesizlik["date"].apply(lambda d: d[:10]))
-            df_dengesizlik.rename(index=str, columns={"positiveImbalance": "Pozitif Dengesizlik Tutarı (TL)",
-                                                      "negativeImbalance": "Negatif Dengesizlik Tutarı (TL)"},
-                                  inplace=True)
-            df_dengesizlik = df_dengesizlik[
-                ["Tarih", "Saat", "Pozitif Dengesizlik Tutarı (TL)", "Negatif Dengesizlik Tutarı (TL)"]]
-        except __ConnectionError:
-            __logging.error(__param.__requestsConnectionErrorLogging, exc_info=False)
-        except __Timeout:
-            __logging.error(__param.__requestsTimeoutErrorLogging, exc_info=False)
-        except __HTTPError as e:
-            __dogrulama.__check_http_error(e.response.status_code)
-        except __RequestException:
-            __logging.error(__param.__request_error, exc_info=False)
-        except KeyError:
-            return __pd.DataFrame()
-        else:
-            return df_dengesizlik
-
-
-def dengesizlik_miktari_tutari(baslangic_tarihi=__dt.datetime.today().strftime("%Y-%m-%d"),
-                               bitis_tarihi=__dt.datetime.today().strftime("%Y-%m-%d")):
-    """
-    İlgili tarih aralığı için saatlik dengesizlik miktarını ve tutarını vermektedir.
+    İlgili tarih aralığı için dengeden sorumlu grupların oluşturmuş olduğu saatlik enerji dengesizlik miktarı ve
+    tutarlarını vermektedir.
 
     Parametreler
     ------------

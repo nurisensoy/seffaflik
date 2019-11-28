8572ec2478a9ef5202eb36df97cff6b7136163a2
nuri.sensoy <nuri.sensoy@epias.com.tr>
2019-11-28 19:49:45 +0300

    reg cmt

--- seffaflik/__init__.py
+++ seffaflik/__init__.py
@@ -1,18 +1,17 @@
-from seffaflik.elektrik.uretim import eak, gerceklesen, kgup, kudup, organizasyon_veris_cekis_birimleri, \
-    organizasyonlar, santral_cekis_birimleri, tum_organizasyonlar_cekis_birimleri, tum_organizasyonlar_eak, \
-    tum_organizasyonlar_kgup
-from seffaflik.elektrik.santraller import ariza_bakim_bildirimleri, kurulu_guc, santraller, yekdem_kurulu_guc, \
-    yekdem_santralleri
-from seffaflik.elektrik.tuketim import dagitim_bolgeleri, gerceklesen, serbest_tuketici_sayisi, tahmin, uecm, \
-    uecm_serbest_tuketici, uecm_tedarik
-from seffaflik.elektrik.piyasalar.dgp import hacim, smf
-from seffaflik.elektrik.piyasalar.gip import aof, hacim, islem_akisi, islem_hacmi, min_max_fiyatlar, ozet, \
-    teklif_edilen_miktarlar
-from seffaflik.elektrik.piyasalar.gop import arz_talep_egrisi, blok_miktari, fark_tutari, hacim, islem_hacmi, kptf, \
-    organizasyonel_hacim, ptf, tum_organizasyonlar_arz_hacim, tum_organizasyonlar_net_hacim, \
-    tum_organizasyonlar_talep_hacim
-from seffaflik.elektrik.piyasalar.ia import hacim, organizasyonel_hacim, tum_organizasyonlar_hacim
-from seffaflik.elektrik.piyasalar.genel import katilimci_sayisi, piyasa_hacimleri
-from seffaflik.elektrik.piyasalar.yanhizmetler import primer_frekans_rezerv_miktari, primer_rezerv_fiyati, \
-    sekonder_frekans_rezerv_miktari, sekonder_rezerv_fiyati
-from seffaflik.elektrik.piyasalar.dengesizlik import dengesizlik_miktari, dengesizlik_tutari, dengesizlik_miktari_tutari
+# from seffaflik.elektrik.uretim import eak, gerceklesen, kgup, kudup, organizasyon_veris_cekis_birimleri, \
+#     organizasyonlar, tum_organizasyonlar_veris_cekis_birimleri, tum_organizasyonlar_eak, \
+#     tum_organizasyonlar_kgup
+# from seffaflik.elektrik.santraller import ariza_bakim_bildirimleri, kurulu_guc, santraller, yekdem_kurulu_guc, \
+#     yekdem_santralleri, santral_veris_cekis_birimleri
+# from seffaflik.elektrik.tuketim import dagitim_bolgeleri, gerceklesen, serbest_tuketici_sayisi, tahmin, uecm, \
+#     uecm_serbest_tuketici, uecm_donemlik_tedarik
+# from seffaflik.elektrik.piyasalar.dgp import hacim, smf
+# from seffaflik.elektrik.piyasalar.gip import aof, hacim, islem_akisi, islem_hacmi, min_max_fiyatlar, ozet, \
+#     teklif_edilen_miktarlar
+# from seffaflik.elektrik.piyasalar.gop import arz_talep_egrisi, blok_miktari, fark_tutari, islem_hacmi, kptf, \
+#     hacim, ptf, tum_organizasyonlar_hacim
+# from seffaflik.elektrik.piyasalar.ia import hacim, tum_organizasyonlar_hacim
+# from seffaflik.elektrik.piyasalar.genel import katilimci_sayisi, hacim
+# from seffaflik.elektrik.piyasalar.yanhizmetler import primer_frekans_rezerv_miktari, primer_rezerv_fiyati, \
+#     sekonder_frekans_rezerv_miktari, sekonder_rezerv_fiyati
+# from seffaflik.elektrik.piyasalar.dengesizlik import dengesizlik

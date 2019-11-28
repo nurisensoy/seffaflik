7482a5a751f1afad68330eaca14ceaac0fbb4127
nuri.sensoy <nuri.sensoy@epias.com.tr>
2019-11-28 19:46:40 +0300

    santral_veris_cekis_birimleri, tum_santraller_veris_cekis_birimleri, gercek_zamanli_uretim_yapan_santraller are added

--- seffaflik/elektrik/santraller.py
+++ seffaflik/elektrik/santraller.py
@@ -16,7 +16,7 @@ __headers = __api.HEADERS
 
 def santraller(tarih=__dt.datetime.now().strftime("%Y-%m-%d")):
     """
-    İlgili tarihte EPİAŞ sistemine kayıtlı santral bilgilerini vermektedir.
+    İlgili tarihte EPİAŞ sistemine kayıtlı santrallerin bilgilerini vermektedir.
 
     Parametre
     ----------
@@ -48,6 +48,102 @@ def santraller(tarih=__dt.datetime.now().strftime("%Y-%m-%d")):
             return __pd.DataFrame()
         else:
             return df_santral
+
+
+def santral_veris_cekis_birimleri(tarih=__dt.datetime.today().strftime("%Y-%m-%d"), santral_id=""):
+    """
+    İlgili tarih ve santral ID için santralin altında tanımlanmış uzlaştırmaya
+    esas veriş-çekiş birim (UEVÇB) bilgilerini vermektedir.
+
+    Parametreler
+    ------------
+    tarih      : %YYYY-%AA-%GG formatında tarih (Varsayılan: bugün)
+    santral_id : metin yada tam sayı formatında santral id (Varsayılan: "")
+
+    Geri Dönüş Değeri
+    -----------------
+    İlgili  UEVÇB Bilgileri(Id, Adı, EIC Kodu)
+    """
+
+    if __dogrulama.__tarih_id_dogrulama(tarih, santral_id):
+        try:
+            resp = __requests.get(__transparency_url + "uevcb?period=" + tarih +
+                                  "&powerPlantId=" + str(santral_id), headers=__headers, timeout=__param.__timeout)
+            resp.raise_for_status()
+            list_unit = resp.json()["body"]["uevcbList"]
+            df_unit = __pd.DataFrame(list_unit)
+            df_unit.rename(index=str, columns={"id": "Id", "name": "Adı", "eic": "EIC Kodu"}, inplace=True)
+            df_unit = df_unit[["Id", "Adı", "EIC Kodu"]]
+        except __ConnectionError:
+            __logging.error(__param.__requestsConnectionErrorLogging, exc_info=False)
+        except __Timeout:
+            __logging.error(__param.__requestsTimeoutErrorLogging, exc_info=False)
+        except __HTTPError as e:
+            __dogrulama.__check_http_error(e.response.status_code)
+        except __RequestException:
+            __logging.error(__param.__request_error, exc_info=False)
+        except KeyError:
+            return __pd.DataFrame()
+        else:
+            return df_unit
+
+
+def tum_santraller_veris_cekis_birimleri(tarih=__dt.datetime.today().strftime("%Y-%m-%d")):
+    """
+    İlgili tarih için EPİAŞ sistemine kayıtlı tüm santrallerin altında tanımlanmış uzlaştırmaya
+    esas veriş-çekiş birim (UEVÇB) bilgilerini vermektedir.
+
+    Parametreler
+    ------------
+    tarih      : %YYYY-%AA-%GG formatında tarih (Varsayılan: bugün)
+
+    Geri Dönüş Değeri
+    -----------------
+    EPİAŞ Sistemine Kayıtlı Santraller ve UEVÇB Bilgileri("Santral Id, "Santral Adı", "Santral EIC Kodu",
+    "Santral Kısa Adı", "UEVÇB Id", "UEVÇB Adı", "UEVÇB EIC Kodu")
+    """
+    if __dogrulama.__tarih_dogrulama(tarih):
+        list_santral = santraller()[["Id", "Adı", "EIC Kodu", "Kısa Adı"]].to_dict("records")
+        santral_len = len(list_santral)
+        list_date_santral_id = list(zip([tarih] * santral_len, list_santral))
+        list_date_santral_id = list(map(list, list_date_santral_id))
+        with __Pool(__mp.cpu_count()) as p:
+            list_df_unit = p.starmap(__santral_veris_cekis_birimleri, list_date_santral_id, chunksize=1)
+        return __pd.concat(list_df_unit).reset_index(drop=True)
+
+
+def gercek_zamanli_uretim_yapan_santraller():
+    """
+    İsteğin yapıldığı tarihte gerçek zamanlı üretim yapan UEVÇB bazında santral bilgilerini vermektedir.
+
+    Parametre
+    ----------
+
+    Geri Dönüş Değeri
+    -----------------
+    Santral Bilgileri(Id, Adı, EIC Kodu, Kısa Adı)
+    """
+    try:
+        resp = __requests.get(__transparency_url + "real-time-generation-power-plant-list", headers=__headers,
+                              timeout=__param.__timeout)
+        resp.raise_for_status()
+        list_santral = resp.json()["body"]["powerPlantList"]
+        df_santral = __pd.DataFrame(list_santral)
+        df_santral.rename(index=str, columns={"id": "Id", "name": "Adı", "eic": "EIC Kodu",
+                                              "shortName": "Kısa Adı"}, inplace=True)
+        df_santral = df_santral[["Id", "Adı", "EIC Kodu", "Kısa Adı"]]
+    except __ConnectionError:
+        __logging.error(__param.__requestsConnectionErrorLogging, exc_info=False)
+    except __Timeout:
+        __logging.error(__param.__requestsTimeoutErrorLogging, exc_info=False)
+    except __HTTPError as e:
+        __dogrulama.__check_http_error(e.response.status_code)
+    except __RequestException:
+        __logging.error(__param.__request_error, exc_info=False)
+    except KeyError:
+        return __pd.DataFrame()
+    else:
+        return df_santral
 
 
 def yekdem_santralleri(tarih=__dt.datetime.now().strftime("%Y-%m-%d")):
@@ -263,3 +359,48 @@ def __yekdem_kurulu_guc(tarih):
         return __pd.DataFrame()
     else:
         return df_guc
+
+
+def __santral_veris_cekis_birimleri(tarih, santral):
+    """
+   İlgili tarih ve santral için santralin altında tanımlanmış uzlaştırmaya
+   esas veriş-çekiş birim (UEVÇB) bilgilerini vermektedir.
+
+   Parametreler
+   ------------
+   tarih      : %YYYY-%AA-%GG formatında tarih
+   santral    : dict formatında santral id, Adı, EIC Kodu, Kısa Adı
+
+   Geri Dönüş Değeri
+   -----------------
+   İlgili  UEVÇB Bilgileri(Santral Id, Santral Adı, Santral EIC Kodu, Santral Kısa Adı, UEVÇB Id,
+                           UEVÇB Adı, UEVÇB EIC Kodu)
+   """
+
+    try:
+        resp = __requests.get(__transparency_url + "uevcb?period=" + tarih +
+                              "&powerPlantId=" + str(santral["Id"]), headers=__headers, timeout=__param.__timeout)
+        resp.raise_for_status()
+        list_unit = resp.json()["body"]["uevcbList"]
+        df_unit = __pd.DataFrame(list_unit)
+        df_unit["Santral Id"] = santral["Id"]
+        df_unit["Santral Adı"] = santral["Adı"]
+        df_unit["Santral EIC Kodu"] = santral["EIC Kodu"]
+        df_unit["Santral Kısa Adı"] = santral["Kısa Adı"]
+        df_unit.rename(index=str,
+                       columns={"id": "UEVÇB Id", "name": "UEVÇB Adı", "eic": "UEVÇB EIC Kodu"},
+                       inplace=True)
+        df_unit = df_unit[["Santral Id", "Santral Adı", "Santral EIC Kodu", "Santral Kısa Adı", "UEVÇB Id",
+                           "UEVÇB Adı", "UEVÇB EIC Kodu"]]
+    except __ConnectionError:
+        __logging.error(__param.__requestsConnectionErrorLogging, exc_info=False)
+    except __Timeout:
+        __logging.error(__param.__requestsTimeoutErrorLogging, exc_info=False)
+    except __HTTPError as e:
+        __dogrulama.__check_http_error(e.response.status_code)
+    except __RequestException:
+        __logging.error(__param.__request_error, exc_info=False)
+    except KeyError:
+        return __pd.DataFrame()
+    else:
+        return df_unit

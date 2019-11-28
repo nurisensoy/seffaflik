358569a804a9bb7b1f702ff9be5a5ec63ab1d0f8
nuri.sensoy <nuri.sensoy@epias.com.tr>
2019-11-28 19:47:03 +0300

    updates

--- seffaflik/ortak/dogrulama.py
+++ seffaflik/ortak/dogrulama.py
@@ -17,6 +17,25 @@ def __baslangic_bitis_tarih_dogrulama(baslangic_tarihi, bitis_tarihi):
         return True
 
 
+def __baslangic_bitis_tarih__periyot_dogrulama(baslangic_tarihi, bitis_tarihi, periyot):
+    try:
+        ilk = __datetime.datetime.strptime(baslangic_tarihi, '%Y-%m-%d')
+        son = __datetime.datetime.strptime(bitis_tarihi, '%Y-%m-%d')
+        if ilk > son:
+            __logging.warning("Bitiş tarihi başlangıç tarihinden küçük olmamalıdır!")
+            return False
+        if periyot.lower() not in ["saatlik", "gunluk", "aylik", "yillik"]:
+            __logging.warning(
+                "Periyot değeri metin formatında\n\t1) saatlik\n\t2) gunluk\n\t3) aylik\n\t4) yillik\nseçeneklerinden "
+                "biri olmalıdır!")
+            return False
+    except ValueError:
+        __logging.warning("Girilen tarihler YYYY-AA-GG formatında olmalıdır! (Ör: 2000-10-25)")
+        return False
+    else:
+        return True
+
+
 def __baslangic_bitis_tarih_eic_dogrulama(baslangic_tarihi, bitis_tarihi, eic):
     try:
         ilk = __datetime.datetime.strptime(baslangic_tarihi, '%Y-%m-%d')
@@ -26,6 +45,36 @@ def __baslangic_bitis_tarih_eic_dogrulama(baslangic_tarihi, bitis_tarihi, eic):
             return False
         if not isinstance(eic, str):
             __logging.warning("Organizasyon EIC değeri metin formatında girilmelidir!")
+            return False
+    except ValueError:
+        __logging.warning("Girilen tarihler YYYY-AA-GG formatında olmalıdır! (Ör: 2000-10-25)")
+        return False
+    else:
+        return True
+
+
+def __baslangic_bitis_tarih_id_dogrulama(baslangic_tarihi, bitis_tarihi, santral_id):
+    try:
+        ilk = __datetime.datetime.strptime(baslangic_tarihi, '%Y-%m-%d')
+        son = __datetime.datetime.strptime(bitis_tarihi, '%Y-%m-%d')
+        if ilk > son:
+            __logging.warning("Bitiş tarihi başlangıç tarihinden küçük olmamalıdır!")
+            return False
+        if not isinstance(santral_id, str) and not isinstance(santral_id, int):
+            __logging.warning("Santral ID değeri metin yada tam sayı formatında girilmelidir!")
+            return False
+    except ValueError:
+        __logging.warning("Girilen tarihler YYYY-AA-GG formatında olmalıdır! (Ör: 2000-10-25)")
+        return False
+    else:
+        return True
+
+
+def __tarih_id_dogrulama(tarih, santral_id):
+    try:
+        __datetime.datetime.strptime(tarih, '%Y-%m-%d')
+        if not isinstance(santral_id, str) and not isinstance(santral_id, int):
+            __logging.warning("Santral ID değeri metin yada tam sayı formatında girilmelidir!")
             return False
     except ValueError:
         __logging.warning("Girilen tarihler YYYY-AA-GG formatında olmalıdır! (Ör: 2000-10-25)")

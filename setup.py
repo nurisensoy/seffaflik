e70a4e07c5a3e206095577680df02372edeaf31b
nuri.sensoy <nuri.sensoy@enerjisauretim.com>
2021-11-30 16:36:57 +0300

    github action

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
 from setuptools import setup, find_packages
+from os import path
 
-with open("README.md", encoding="utf-8") as f:
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
+
+# Get the version from the source code
+with open(path.join(here, 'seffaflik', '__init__.py'), encoding='utf-8') as f:
+    lines = f.readlines()
+    for l in lines:
+        if l.startswith('__version__'):
+            __version__ = l.split('"')[1]  # take the part after the first "
 
 setup(
     name="seffaflik",
-    version="0.0.17",
+    version=__version__,
     author='Dr. Nuri Şensoy',
     author_email='nuri.sensoy@enerjisauretim.com.tr',
     description="EPİAŞ tarafından Şeffaflık Platformunda yayımlanmakta olan verileri çekmek için tasarlanmış Python "
@@ -15,11 +26,10 @@ setup(
     keywords=['seffaflik', 'transparency', 'Turkish Energy Markets'],
     url='https://github.com/nurisensoy/seffaflik',
     packages=find_packages(),
-    install_requires=[
-        'requests',
-        'pandas',
-        'python-dateutil',
-    ],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed.
+    install_requires=['requests', 'pandas', 'python-dateutil', 'beautifulsoup4', 'xlrd'],
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
@@ -28,4 +38,7 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    package_data={
+        'seffaflik': ['LICENSE', 'README.md'],
+    },
 )
